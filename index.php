9cad143049195efd107173f05f9bd98d6fd6cfd8
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2020-02-20 14:09:14 -0600

    Pruebas de la libreria DBManager
    Si poseen registros en las tablas clientes y usuarios podran visualizar ciertos datos en el index por ahora

--- index.php
+++ index.php
@@ -9,8 +9,28 @@
 
 <?php
 	include "DB/DBManager.php";
+	echo "<h1>Conexión Basica a tabla clientes:</h1>";
+	$coneccion= new tableClientsManager();
+	$resultado=$coneccion->getAllClients();
+    echo " with {$resultado->rowCount()} registers <br><br>";
+	foreach ($resultado as $campo){
+	    echo "Nombre: " . $campo["Nombre"] . " Apellido Paterno: " . $campo["Apellido_P"] . "<br>";
+    }
 
-	$coneccion= new DBManager();
+
+    echo "<h1>Conexión Basica a tabla Usuarios:</h1>";
+    $coneccion= new tableUserManager();
+    $Nivel = new tableNivelUsuario();
+    $resultado=$coneccion->getAllUsers();
+    echo " with {$resultado->rowCount()} registers <br><br>";
+    foreach ($resultado as $campo){
+
+
+        echo "Nickname: " . $campo["IDUsuario"] . " Nivel: " . $Nivel->NivelUsuarioToText($campo["NivelUsuario"]) . "<br>";
+    }
+
+
+
 
 
 
