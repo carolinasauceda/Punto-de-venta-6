a9391b2d06236773b6d671e7d384a986a9e16c9b
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2020-02-25 12:18:14 -0600

    Actualización del archivo commonHeaders
    Caro, cualquier tipo de estilo, o javascript que sea necesario incluir en la cabecera, y sepas que lo vas a usar en muchos archivos, favor de colocarlos en este archivo y mandarlo llamar con php. Así te evitas copiar y pegar muchos datos

--- common/commonHeaders.php
+++ common/commonHeaders.php
@@ -1,2 +1,4 @@
 <!--BootstrapCDN-->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">-->
+<link href="https://fonts.googleapis.com/css?family=Hind&display=swap" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
