ab1fc64f1288b91609d657735c83259f5f182e09
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2020-02-25 17:00:03 -0600

    Actualización del controllador
    Solo agregue unas cuantas lineas y modifique otras de lugar

--- frontend/Controllers/loginController.php
+++ frontend/Controllers/loginController.php
@@ -1,5 +1,5 @@
 <?php
-require "../DB/DBManager.php";
+require "../../DB/DBManager.php";
 
 class loginManager extends  DBManager {
 
@@ -8,20 +8,20 @@ class loginManager extends  DBManager {
         parent::__construct();
     }
 
-    public function actionLogin(){
+    public function actionLogin($login,$password ){
         try{
             $this->sql="SELECT * FROM Empleados where IDEmpleado= :login AND Clave= :password";
             $resultado=$this->base->prepare($this->sql);
-            $login=htmlentities(addslashes($_POST["usuario"]));
-            $password=htmlentities(addslashes($_POST["password"]));
             $resultado->bindValue(":login",$login);
             $resultado->bindValue(":password",$password);
             $resultado->execute();
             $numero_registro=$resultado->rowCount();
             if($numero_registro!=0){
-                header("location:index.php");
+                //header("location:../index.php");
+                return 1;
             }else{
-                header("location:login.php");
+                //header("location:../login.php");
+                return 0;
             }
         }catch(Exception $e){
             die("Error: " . $e->getMessage());
@@ -31,5 +31,13 @@ class loginManager extends  DBManager {
 
 }
 
+$loginObj= new loginManager();
+
+if(isset($_POST['usernamefield'])){
+    $login=htmlentities(addslashes($_POST["usernamefield"]));
+    $password=htmlentities(addslashes($_POST["passwordfield"]));
+    echo $loginObj->actionLogin($login, $password);
+}
 
 ?>
+
