e8726635f0e4bd2369696990e94d81b73538d945
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2020-02-25 23:38:36 -0600

    Modificación del Index
    Solamente agregue unas lineas para ver si entraba o no

--- frontend/index.php
+++ frontend/index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php
+    require "../common/Controllers/sessionController.php"
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <p>Este es el index del frontend</p>
+    <p>Bienvenido al Frontend: <?php echo $_SESSION["Nombre"] . " " . $_SESSION["ApellidoP"] . " " . $_SESSION["ApellidoM"]?></p>
+    <a href="login.php">Logout</a>
 </body>
 </html>
