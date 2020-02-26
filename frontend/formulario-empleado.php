07149e886a468cdf77835c6d18ea27c9918e3aad
Carolina Sauceda <k98@live.com>
2020-02-26 17:48:04 -0600

    formularios para edicion/llenado por usuario

---
+++ frontend/formulario-empleado.php
@@ -0,0 +1,165 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+
+
+    <link rel="icon" href="Favicon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <title>Nuevo Empleado</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
+    <div class="container">
+    <a class="navbar-brand" href="#">L O G O</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">INICIO</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">SALIR</a>
+            </li>
+        </ul>
+
+    </div>
+    </div>
+</nav>
+
+<main class="my-form">
+    <div class="cotainer">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Nuevo empleado</div>
+                        <div class="card-body">
+                            <form name="my-form" onsubmit="return validform()" action="success.php" method="">
+                                <div class="form-group row">
+                                    <label for="full_name" class="col-md-4 col-form-label text-md-right">RFC</label>
+                                    <div class="col-md-6">
+                                        <input type="text" id="id" class="form-control" name="id">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="full_name" class="col-md-4 col-form-label text-md-right">Nombre</label>
+                                    <div class="col-md-6">
+                                        <input type="text" id="nombre" class="form-control" name="nombre">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="email_address" class="col-md-4 col-form-label text-md-right">Apellido Paterno</label>
+                                    <div class="col-md-6">
+                                        <input type="text" id="apellido_p" class="form-control" name="apellido_p">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="user_name" class="col-md-4 col-form-label text-md-right">Apellido Materno</label>
+                                    <div class="col-md-6">
+                                        <input type="text" id="apellido_m" class="form-control" name="apellido_m">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="phone_number" class="col-md-4 col-form-label text-md-right">Fecha de nacimiento</label>
+                                    <div class="col-md-6">
+                                        <input type="date" class="form-control" id="correo" name="correo">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="permanent_address" class="col-md-4 col-form-label text-md-right">Contratacion</label>
+                                    <div class="col-md-6">
+                                        <input type="date" id="telefono" class="form-control" name="telefono">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="permanent_address" class="col-md-4 col-form-label text-md-right">Direccion</label>
+                                    <div class="col-md-6">
+                                        <input type="text" id="telefono" class="form-control" name="telefono">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="permanent_address" class="col-md-4 col-form-label text-md-right">Telefono</label>
+                                    <div class="col-md-6">
+                                        <input type="tel" id="telefono" class="form-control" name="telefono">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="permanent_address" class="col-md-4 col-form-label text-md-right">Tipo de usuario</label>
+                                    <div class="col-md-6">
+                                        <input list="user-type" id="telefono" class="form-control" name="telefono">
+                                        <datalist id="user-type">
+                                            <option value="Administrador">
+                                            <option value="Vendedor">
+                                            <option value="Almacenista">
+                                        </datalist>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="permanent_address" class="col-md-4 col-form-label text-md-right">Clave</label>
+                                    <div class="col-md-6">
+                                        <input type="password" id="telefono" class="form-control" name="telefono">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="permanent_address" class="col-md-4 col-form-label text-md-right">Estado</label>
+                                    <div class="col-md-6">
+                                        <input list="estado" id="estado-seleccion" class="form-control" name="estado-seleccion">
+                                        <datalist id="estado">
+                                            <option value="activo">
+                                            <option value="archivado">
+                                          </datalist>
+                                    </div>
+                                </div>
+
+
+                                <!--<div class="form-group row">
+                                    <label for="nid_number" class="col-md-4 col-form-label text-md-right"><abbr
+                                                title="National Id Card">NID</abbr> Number</label>
+                                    <div class="col-md-6">
+                                        <input type="text" id="nid_number" class="form-control" name="nid-number">
+                                    </div>
+                                </div>-->
+
+                                    <div class="col-md-6 offset-md-4">
+                                        <button type="submit" class="btn btn-primary">
+                                        Guardar
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ frontend/formulario-producto.php
@@ -0,0 +1,151 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+
+
+    <link rel="icon" href="Favicon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <title>Nuevo Producto</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
+    <div class="container">
+    <a class="navbar-brand" href="#">L O G O</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">INICIO</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">SALIR</a>
+            </li>
+        </ul>
+
+    </div>
+    </div>
+</nav>
+
+<main class="my-form">
+    <div class="cotainer">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Nuevo producto</div>
+                        <div class="card-body">
+                            <form name="my-form" onsubmit="return validform()" action="success.php" method="">
+                                <div class="form-group row">
+                                    <label for="full_name" class="col-md-4 col-form-label text-md-right">Nombre</label>
+                                    <div class="col-md-6">
+                                        <input type="text" id="nombre" class="form-control" name="nombre">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="email_address" class="col-md-4 col-form-label text-md-right">Proveedor</label>
+                                    <div class="col-md-6">
+                                        <input list="browsers" id="proveedor" class="form-control name="proveedor">
+                                        <datalist id="browsers">
+                                            <option value="Bimbo">
+                                            <option value="Coca cola">
+                                            <option value="Pepsi">
+                                            <option value="Sabritas">
+                                            <option value="Gamesa">
+                                          </datalist>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="user_name" class="col-md-4 col-form-label text-md-right">Categoria</label>
+                                    <div class="col-md-6">
+                                        <input list="categoria" id="categorias" class="form-control name="categorias">
+                                        <datalist id="categoria">
+                                            <option value="Bebida">
+                                            <option value="Pan dulce">
+                                            <option value="Jugo natural">
+                                            <option value="Galletas">
+                                            <option value="Dulces">
+                                          </datalist>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="phone_number" class="col-md-4 col-form-label text-md-right">Precio individual</label>
+                                    <div class="col-md-6">
+                                        <input type="number" class="form-control min="0.00" max="10000.00" step="0.01" id="precio" name="precio">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="present_address" class="col-md-4 col-form-label text-md-right">Stock</label>
+                                    <div class="col-md-6">
+                                        <input type="number" id="stock" name="stock" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="permanent_address" class="col-md-4 col-form-label text-md-right">Imagen</label>
+                                    <div class="col-md-6">
+                                        <input type="file" id="imagen" class="form-control" name="imagen">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="permanent_address" class="col-md-4 col-form-label text-md-right">Estado</label>
+                                    <div class="col-md-6">
+                                        <input list="estado" id="estado-seleccion" class="form-control" name="estado-seleccion">
+                                        <datalist id="estado">
+                                            <option value="activo">
+                                            <option value="archivado">
+                                          </datalist>
+                                    </div>
+                                </div>
+
+
+                                <!--<div class="form-group row">
+                                    <label for="nid_number" class="col-md-4 col-form-label text-md-right"><abbr
+                                                title="National Id Card">NID</abbr> Number</label>
+                                    <div class="col-md-6">
+                                        <input type="text" id="nid_number" class="form-control" name="nid-number">
+                                    </div>
+                                </div>-->
+
+                                    <div class="col-md-6 offset-md-4">
+                                        <button type="submit" class="btn btn-primary">
+                                        Guardar
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</body>
+</html>
