de5b1e9c9bf6cf40c926815fa447292ae2d05653
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2020-02-28 23:41:34 -0600

    Creación de menu sencillito para probar cositas

--- frontend/index.php
+++ frontend/index.php
@@ -11,5 +11,13 @@
 <body>
     <p>Bienvenido al Frontend: <?php echo $_SESSION["Nombre"] . " " . $_SESSION["ApellidoP"] . " " . $_SESSION["ApellidoM"]?></p>
     <a href="login.php">Logout</a>
+    <br>
+    <br> <a href="formulario-categoria.php">Formulario categoria</a>
+    <br> <a href="formulario-cliente.php">Formulario cliente</a>
+    <br> <a href="formulario-empleado.php">Formulario empleado</a>
+    <br> <a href="formulario-nivel-usuario.php">Formulario nivel de usuario</a>
+    <br> <a href="formulario-producto.php">Formulario producto</a>
+    <br> <a href="formulario-proveedor.php">Formulario proveedores</a>
+
 </body>
 </html>
