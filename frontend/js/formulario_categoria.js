9a32daf1af2bbee7d3b751261daedbb330d0cb4c
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2020-02-29 17:23:34 -0600

    Conector ajax para el controllador y el formulario categoria

---
+++ frontend/js/formulario_categoria.js
@@ -0,0 +1,37 @@
+var buttonpressed;
+$('.actionbutton').click(function() {
+    buttonpressed = $(this).attr('id');
+});
+
+$(document).ready(function(){
+    $('#formulario-categoria').submit(function (e){
+        const postData={
+            id:$("#nombre").attr("data"),
+            nombre: $('#nombre').val(),
+            descripcion:$('#descripcion').val(),
+            activo:$('#estado-seleccion').val(),
+        };
+        console.log(postData);
+        if(buttonpressed=="btnSave"){
+
+            $.post("Controllers/formCategoriaController.php",postData,function(response){
+                //console.log("Server response: "+response.toString());
+                console.log(response);
+                if(response==1){
+
+                    alert("Informacion guardada o actualizada correctamente");
+                }else{
+                    //Colocar aquí tu magia xd
+                    alert("Verefica que toda información es correctamente llenada");
+                }
+            });
+
+        }else if(buttonpressed="btnlol"){
+            console.log("Lol press ");
+        }
+        e.preventDefault();
+    });
+
+    $('#btnlol').sum
+});
+
