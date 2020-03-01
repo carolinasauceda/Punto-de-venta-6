25f7ec4198e69ae30f0380f25064f7deed42019e
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2020-03-01 15:21:37 -0600

    Ajustes finales al conector de AJAX del formulario Categoria

--- frontend/index.php
+++ frontend/index.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php
-    require "../common/Controllers/sessionController.php"
-    ?>
+    <?php  require "../common/Controllers/sessionController.php" ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
--- frontend/js/formulario_categoria.js
+++ frontend/js/formulario_categoria.js
@@ -3,6 +3,9 @@ $('.actionbutton').click(function() {
     buttonpressed = $(this).attr('id');
 });
 
+
+ var ConnectionString="Controllers/formCategoriaController.php";
+
 $(document).ready(function(){
     loadregister();
 
@@ -10,11 +13,18 @@ $(document).ready(function(){
         const postData={
             initData:$("#nombre").attr("data"),
         };
-        $.post("Controllers/formCategoriaController.php",postData,function(ServerResponse){
-            console.log(ServerResponse);
+        $.post(ConnectionString,postData,function(ServerResponse){
+            if(ServerResponse!=0){
+                let responde=JSON.parse(ServerResponse);
+                console.log(responde);
+                $("#nombre").attr("data",responde['id']);
+                $('#nombre').attr('value',responde['Nombre']);
+                $('#descripcion').attr('value',responde['Descripcion']);
+                $('#estado-seleccion').val(responde['RActivo']);
+            }
+
         });
     }
-
 
     $('#formulario-categoria').submit(function (e){
         const postData={
@@ -26,20 +36,32 @@ $(document).ready(function(){
         console.log(postData);
         if(buttonpressed=="btnSave"){
 
-            $.post("Controllers/formCategoriaController.php",postData,function(response){
+            $.post(ConnectionString,postData,function(response){
                 //console.log("Server response: "+response.toString());
                 console.log(response);
                 if(response==1){
 
-                    alert("Informacion guardada o actualizada correctamente");
+                    $('#SuccessAlert').attr('class',"alert alert-success alert-dismissible d-block");
                 }else{
                     //Colocar aquí tu magia xd
-                    alert("Verefica que toda información es correctamente llenada");
+                    $('#WarningAlert').attr('class',"alert alert-warning alert-dismissible d-block");
                 }
             });
 
-        }else if(buttonpressed="btnlol"){
-            console.log("Lol press ");
+        }else if(buttonpressed=="btnDelete"){
+            var r= confirm("Estas apunto de eliminar este registro \n ¿Estas seguro de ello?");
+            if(r){
+               $.post(ConnectionString,{deleteAction:1, id:$("#nombre").attr("data"),},function(response){
+                   if(response==0){
+                       alert("Este registro no existe o ha sido borrado previamente");
+                       window.location.href="/frontend/formulario-categoria.php";
+                   }else if(response==1){
+                        $('#SuccessAlert').attr('class',"alert alert-success alert-dismissible d-block");
+                   }
+               })
+            }
+        }else if(buttonpressed=="btnAddNew"){
+            window.location="/frontend/formulario-categoria.php";
         }
         e.preventDefault();
     });
