24a0789cfa254c0d41954475788268334b5a7e2b
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2020-03-25 16:55:27 -0500

    Creación de la clase sessionController
    Esta servira para manejar la seguridad, autorizaciones de usuarios y aspectos relevantes a las sesiones activas

--- common/Controllers/sessionController.php
+++ common/Controllers/sessionController.php
@@ -1,6 +1,54 @@
 <?php
-session_start();
-if(!isset($_SESSION["usuario"])){
-    header("location:login.php");
-}
+
+    class sessionController{
+
+        private $serverSettings;
+
+        public function __construct($path2root)
+        {
+            //Path2Root from the file that is instanciating this class
+            require  $path2root.'config/serverSettings.php';
+            session_start();
+            $this->serverSettings =new serverSettings();
+            if(!isset($_SESSION["usuario"])){
+                header("location:" . $this->serverSettings->getServerDomain(). "/backend/views/login.php" );
+            }
+        }
+
+        public function isAutorizeFor($page){
+            $permisosNivel=(int) $_SESSION['AutorizacionNivel'];
+            switch($page){
+                case 'formClientes':
+                case 'formEmpleados':
+                case 'formNivelUsuario':
+                    if($permisosNivel<100) $this->_notAutorizeRedirect();
+                    break;
+
+                case 'formProductos':
+                    if($permisosNivel<=20) return 0;
+                    break;
+
+                case 'formProveedor':
+                case 'formCategoria':
+                if($permisosNivel<=20) $this->_notAutorizeRedirect(); else return 0;
+                    break;
+            }
+
+            //Si llegue hasta aqui esta Autorizado con permisos de escritura/lectura
+            return 1;
+        }
+
+        public function _redirec(){
+            header("Location: ");
+            die();
+        }
+
+        public function _notAutorizeRedirect(){
+            header("location:" . $this->serverSettings->getServerDomain(). "/backend/views/index.php" );
+        }
+
+
+
+
+    }
 ?>
